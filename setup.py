2cb46585bd39c2b1dd372783ccdc34d8922ee2d8
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-10-02 16:42:35 +0530

    finalchanges

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@ def get_requirements_list()->List[str]:
     from the requirements.txt file.
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
+        requirement_list = [lib_name.replace("\n","") for lib_name in requirement_file.readlines()]
+        print(requirement_list)
+        if "-e ." in requirement_list:
+            requirement_list.remove('-e .')
         return requirement_list
 
 setup(
