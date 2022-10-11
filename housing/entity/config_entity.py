d1f4be53e82903791b65953477f0b2ae27333abd
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-10-11 07:47:30 +0530

    updated

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -9,7 +9,7 @@ DataIngestionConfig = namedtuple("DataIngestionConfig", ["dataset_download_url",
                                                          )
 
 
-DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path"])
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path", "report_file_path", "report_page_file_path"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room", 
                                                                    "transformed_train_dir", 
