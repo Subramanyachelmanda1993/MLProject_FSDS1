b22714616b04cd0b0bf1fc66ce29626a58279731
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-10-17 09:34:34 +0530

    updated

--- demo.py
+++ demo.py
@@ -7,10 +7,10 @@ from housing.config.configuration import Configuartion
 
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-        # data_validation_config = Configuartion().get_data_validation_config()
-        # print(data_validation_config)
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_transformation_config = Configuartion().get_data_transformation_config()
+        print(data_transformation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
